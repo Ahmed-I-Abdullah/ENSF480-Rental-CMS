dd9c8c2a2b54149ce0d18b0de25a4e873e0f8276
Ahmed-I-Abdullah <ahmedi3aaa@hotmail.com>
2021-12-01 23:20:50 -0700

    feat: add password hashing

--- src/main/App.java
+++ src/main/App.java
@@ -2,6 +2,7 @@ package src.main;
 
 import src.main.controller.ControllerManager;
 import src.main.controller.AdminController;
+import src.main.controller.UserController;
 import src.main.model.user.Manager;
 import java.io.FileNotFoundException;
 import java.util.Date;
@@ -17,6 +18,15 @@ public class App {
             long t = System.currentTimeMillis() - (3*24*60*60*1000);
             long t2 = System.currentTimeMillis() + (3*24*60*60*1000);
             am.generateReport(new Date(t), new Date(t2));
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.exit(-1);
+        }
+
+        UserController um = new UserController(m);
+        try {
+            um.hash_password("12345678");
+            um.hash_password("12345678");
         } catch (Exception e) {
             e.printStackTrace();
             System.exit(-1);
--- src/main/controller/UserController.java
+++ src/main/controller/UserController.java
@@ -1,19 +1,42 @@
 package src.main.controller;
 
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import src.main.model.user.User;
 
 public class UserController {
-    private User user;
 
-    public UserController(User u) {
-        this.user = u;
-    }
+  private User user;
 
-    public void setUser(User u) {
-        this.user = u;
-    }
+  public UserController(User u) {
+    this.user = u;
+  }
 
-    public User getUser() {
-        return user;
+  public void setUser(User u) {
+    this.user = u;
+  }
+
+  public User getUser() {
+    return user;
+  }
+
+  public boolean signUp(String email, String name, String password) {
+      return true;
+  }
+
+  public String hash_password(String password) throws NoSuchAlgorithmException {
+    StringBuilder hashedPassword = new StringBuilder();
+    String salt = "!15wRP/8N:F8*uu5A>?4"; // should probably be in environment variables
+    MessageDigest md = MessageDigest.getInstance("SHA-512");
+    md.update(salt.getBytes(StandardCharsets.UTF_8));
+    byte[] bytes = md.digest(password.getBytes(StandardCharsets.UTF_8));
+    for (int i = 0; i < bytes.length; i++) {
+      hashedPassword.append(
+        Integer.toString((bytes[i] & 0xff) + 0x100, 16).substring(1)
+      );
     }
+    System.out.println("Hashed password is: " + hashedPassword.toString());
+    return hashedPassword.toString();
+  }
 }
