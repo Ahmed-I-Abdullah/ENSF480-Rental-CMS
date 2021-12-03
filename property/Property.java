eb5f78675de19a1704f447d97a185d4f05638a94
hudaabbas <73191261+hudaabbas@users.noreply.github.com>
2021-12-03 00:34:16 -0700

    Property.java (need user pckg)

---
+++ property/Property.java
@@ -0,0 +1,74 @@
+package property;
+import java.util.*;
+//import user.Landlord; when connected package add this
+
+public class Property{
+    private int houseID;
+    private Address address;
+    private ListingDetails specifications;
+    private Landlord postedBy;
+    private Date datePosted;
+    private String description;
+
+    public Property(int houseID, Address address, ListingDetails specifications, Landlord postedBy, Date datePosted, String description) {
+        this.houseID = houseID;
+        this.address = address;
+        this.specifications = specifications;
+        this.postedBy = postedBy;
+        this.datePosted = datePosted;
+        this.description = description;
+    }
+
+    public listingState checkState(){
+        return specifications.getState();
+    }
+
+    public int getHouseID() {
+        return this.houseID;
+    }
+
+    public void setHouseID(int houseID) {
+        this.houseID = houseID;
+    }
+
+    public Address getAddress() {
+        return this.address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+
+    public ListingDetails getSpecifications() {
+        return this.specifications;
+    }
+
+    public void setSpecifications(ListingDetails specifications) {
+        this.specifications = specifications;
+    }
+
+    public Landlord getPostedBy() {
+        return this.postedBy;
+    }
+
+    public void setPostedBy(Landlord postedBy) {
+        this.postedBy = postedBy;
+    }
+
+    public Date getDatePosted() {
+        return this.datePosted;
+    }
+
+    public void setDatePosted(Date datePosted) {
+        this.datePosted = datePosted;
+    }
+
+    public String getDescription() {
+        return this.description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+}
