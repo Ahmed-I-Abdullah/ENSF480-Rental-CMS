468b8ba12741dc020cd8c3a7176e27a1e9bbb38a
hudaabbas <73191261+hudaabbas@users.noreply.github.com>
2021-12-06 21:06:32 -0700

    Change Listing State button
    - id currently hardcoded, not sure how to pass the houses id when view is clicked

--- src/main/view/ListingsPage.java
+++ src/main/view/ListingsPage.java
@@ -2,10 +2,11 @@ package src.main.view;
 
 import java.awt.Graphics;
 import javax.swing.*;
+import java.awt.*;
+
 import src.main.model.user.UserType;
 
 import src.main.controller.ViewController;
-import src.main.controller.AdminController;
 import src.main.model.property.*;
 
 public class ListingsPage extends Page {
@@ -28,6 +29,62 @@ public class ListingsPage extends Page {
                     resetSwitchEvent();
                 });
 
+        String id = "2e535bb8-54cd-11ec-8d3e-0afc51b797af";
+
+        JLabel state = new JLabel(controller.getPostingController().getListingState(id).toString());
+        state.setBounds(375, 310, 100, 30);
+
+        if (
+        controller.getUserController().getAuthenticatedUser() != null &&
+        (controller.getUserController().getAuthenticatedUser().getUserType() ==
+        UserType.LANDLORD || controller.getUserController().getAuthenticatedUser().getUserType() ==
+        UserType.MANAGER)
+        ) {
+            JButton filters = new JButton("Change Listing State");
+            filters.setBounds(475, 310, 200, 30);
+
+            filters.addActionListener(
+            e -> {
+                JFrame pop = new JFrame("Select the Listing State");
+                pop.setSize(250, 150);
+                pop.setLocationRelativeTo(null);
+                pop.setLayout(new GridBagLayout());
+
+                GridBagConstraints c = new GridBagConstraints();
+                c.fill = GridBagConstraints.HORIZONTAL;
+
+                int chosenState = 0;
+                JComboBox<String> changeListingState = new JComboBox<String>(ListingState.getNames(ListingState.class));
+                changeListingState.setSelectedIndex(chosenState);
+
+                JButton save = new JButton("Save");
+
+                save.addActionListener(
+                p -> {
+                    ListingState new_state = ListingState.valueOf(changeListingState.getSelectedItem().toString());
+                    controller.getPostingController().changeListingState(id, new_state.ordinal());
+                    state.setText(controller.getPostingController().getListingState(id).toString()); //change listing state on main page
+                    pop.setVisible(false);
+                }
+                );
+
+                c.gridwidth = 1;
+                c.weightx = 0.0;
+                c.gridx = 0;
+                c.gridy = 0;
+                pop.add(changeListingState, c);
+
+                c.gridx = 0;
+                c.gridy = 1;
+                pop.add(save, c);
+
+                pop.setVisible(true);
+            }
+            );
+
+            f.add(filters);
+        }
+        f.add(state);
         f.add(back);
         f.getContentPane().add(this);
         f.setVisible(true);
@@ -51,23 +108,7 @@ public class ListingsPage extends Page {
         widget.draw(g);
         widget = new Text(275, 280, "Levels: " + 2);
         widget.draw(g);
-        
-        String id = "2e535bb8-54cd-11ec-8d3e-0afc51b797af";
-        int new_state = 2;
-        //change listing state
-        if (
-        controller.getUserController().getAuthenticatedUser() != null &&
-        (controller.getUserController().getAuthenticatedUser().getUserType() ==
-        UserType.LANDLORD || controller.getUserController().getAuthenticatedUser().getUserType() ==
-        UserType.MANAGER)
-        ) {
-            ListingState current_state = controller.getPostingController().getListingState(id);
-            widget = new Text(275, 330, "Listing State: " + current_state);
-            widget.draw(g);
-            if(ListingState.values()[new_state] != current_state){
-                widget = new Text(275, 370, "New Listing State: " + controller.getPostingController().changeListingState(id, new_state));
-                widget.draw(g);
-            }
-        }
+        widget = new Text(275, 330, "Listing State: ");
+        widget.draw(g);
     }
 }
