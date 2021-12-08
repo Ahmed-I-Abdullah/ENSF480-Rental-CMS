3315fc748d17e1e4e5bd79a51f0cca1b10d508bb
AdamAbouelhassan <78115573+AdamAbouelhassan@users.noreply.github.com>
2021-12-08 16:06:54 -0700

    Documentation

--- src/main/model/user/PostingsNotification.java
+++ src/main/model/user/PostingsNotification.java
@@ -8,14 +8,19 @@ public class PostingsNotification implements Subject {
     private Property latestInsertedProperty;
     private ArrayList<Observer> observers;
 
+    // Promises: add observer to observers array
+    // Requires: observer to be added
     public void registerObserver(Observer o) {
         this.observers.add(o);
     }
 
+    // Promises: remove observer to observers array
+    // Requires: observer to be removed
     public void removeObserver(Observer o) {
         this.observers.remove(o);
     }
 
+    // Promises: notify all observers of new property added to their subscription specifications
     public void notifyAllObservers() {
         for (int i = 0; i < observers.size(); i++) {
             observers.get(i).update(latestInsertedProperty);
