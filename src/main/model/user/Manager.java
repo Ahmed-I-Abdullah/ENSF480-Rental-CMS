27f52f9759de19f13eeb835fb5a48f17bfcdd61f
AdamAbouelhassan <78115573+AdamAbouelhassan@users.noreply.github.com>
2021-12-08 16:03:02 -0700

    Documentation

--- src/main/model/user/Manager.java
+++ src/main/model/user/Manager.java
@@ -7,41 +7,53 @@ public class Manager implements User {
     private String email;
     private ArrayList<Landlord> landlords;
 
+    // Constructor
     public Manager(String email, String name){
         setName(name);
         setEmail(email);
     }
 
+    // name getter
     public String getName() {
         return this.name;
     }
     
+    // name setter
     public void setName(String name){
         this.name = name;
     }
 
+    // email getter
     public String getEmail() {
         return this.email;
     }
     
+    // email setter
     public void setEmail(String email){
         this.email = email;
     }
 
+    // Promises: return specified User
+    // Requires: target User to be accessed
     public User accessUser(User user){
         return user;
     }
 
+    // Promises: add landlord to this manager's landlord array
+    // Requires: landlord to be added
     public int addLandlord(Landlord landlord){
         this.landlords.add(landlord);
         return this.landlords.size();
     }
 
+    // Promises: remove landlord to this manager's landlord array
+    // Requires: landlord to be removed
     public int removeLandlord(Landlord landlord){
         this.landlords.remove(landlord);
         return this.landlords.size();
     }
 
+    // Promises: return user type, which is MANAGER
     public UserType getUserType(){
         return UserType.MANAGER;
     }
