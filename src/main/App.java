d3dc4102c964d0d74cfd3f1821c3eb27a2877748
Ahmed-I-Abdullah <ahmedi3aaa@hotmail.com>
2021-12-08 09:45:24 -0700

    fix: sleep inside switch loops

--- src/main/App.java
+++ src/main/App.java
@@ -1,13 +1,21 @@
 package src.main;
 
 import java.io.FileNotFoundException;
+import java.lang.*;
 import src.main.controller.ControllerManager;
 import src.main.controller.UserController;
 import src.main.controller.ViewController;
 import src.main.view.*;
-import javax.swing.*;
 
 public class App {
+
+  public static void sleep() {
+    try {
+      Thread.sleep(500);
+    } catch (Exception e) {
+      System.out.println();
+    }
+  }
 
   public static void main(String[] args) throws FileNotFoundException {
     ControllerManager.connectDatabase();
@@ -19,50 +27,64 @@ public class App {
     Page p = new MainPage(w, v);
     p.draw();
     boolean firstDisplay = true;
-	
-	int c=0;
+
+    int c = 0;
     while (true) {
-		System.out.println("run #:"+ c);
-		c++;
+      System.out.println("run #:" + c);
+      c++;
       if (p.getSwitchEvent() == 0 && !firstDisplay) {
         p = new MainPage(w, v);
         p.draw();
       }
-      while (p.getSwitchEvent() == 0) {System.out.println("");}
+      while (p.getSwitchEvent() == 0) {
+        sleep();
+      }
       if (p.getSwitchEvent() == 1) {
         p = new BrowseListingsPage(w, v);
         p.draw();
-        while (p.getSwitchEvent() == 1) {System.out.println("");}
+        while (p.getSwitchEvent() == 1) {
+          sleep();
+        }
       }
 
       if (p.getSwitchEvent() == 2) {
         p = new CreateListingsPage(w, v);
         p.draw();
-        while (p.getSwitchEvent() == 2) {System.out.println("");}
+        while (p.getSwitchEvent() == 2) {
+          sleep();
+        }
       }
 
       if (p.getSwitchEvent() == 3) {
         p = new ManagerControlPage(w, v);
         p.draw();
-        while (p.getSwitchEvent() == 3) {System.out.println("");}
+        while (p.getSwitchEvent() == 3) {
+          sleep();
+        }
       }
 
       if (p.getSwitchEvent() == 4) {
         p = new ManagerReportPage(w, v);
         p.draw();
-        while (p.getSwitchEvent() == 4) {System.out.println("");}
+        while (p.getSwitchEvent() == 4) {
+          sleep();
+        }
       }
 
       if (p.getSwitchEvent() == 5) {
         p = new NotificationsPage(w, v);
         p.draw();
-        while (p.getSwitchEvent() == 5) {System.out.println("");}
+        while (p.getSwitchEvent() == 5) {
+          sleep();
+        }
       }
 
       if (p.getSwitchEvent() == 6) {
         p = new ListingsPage(w, v);
         p.draw();
-        while (p.getSwitchEvent() == 6) {System.out.println("");}
+        while (p.getSwitchEvent() == 6) {
+          sleep();
+        }
       }
 
       firstDisplay = false;
